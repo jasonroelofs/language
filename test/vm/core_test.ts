67e34a96271e7f3628ba329b58cb621d2afbc85e
Jason Roelofs <jasongroelofs@gmail.com>
2018-12-10 14:40:15 -0500

    Restructure spec and clean up some testing
    * Move core tests into spec/core, start organizing the specs
    * Delete and reimplement javascript tests into the language spec

--- test/vm/core_test.ts
+++ test/vm/core_test.ts
@@ -5,54 +5,11 @@ import { toObject, True, False } from "@vm/object"
 import { World } from "@vm/core"
 import VM from "@vm/vm"
 
-describe("Object", () => {
-  it("supports basic object equality", () => {
-    let tests = {
-      "true == true": True,
-      "false == false": True,
-      "true == false": False,
-      "true != false": True,
-      "null == null": True
-    }
-
-    for(var test in tests) {
-      assertExpression(test, tests[test])
-    }
-  })
-})
-
-describe("True / False / Null", () => {
-  it("supports toString on these built-ins", () => {
-    assertExpression("true.toString()", toObject("true"))
-    assertExpression("false.toString()", toObject("false"))
-    assertExpression("null.toString()", toObject("null"))
-  })
-})
-
 describe("Number", () => {
-  it("exposes basic arithmetic operations", () => {
-    let tests = {
-      "1 + 1": toObject(2),
-      "1 - 1": toObject(0),
-      "1 * 3": toObject(3),
-      "10 / 2": toObject(5),
-    }
-
-    for(var test in tests) {
-      assertExpression(test, tests[test])
-    }
-  })
-
   it("supports toString to get the Javascript value", () => {
     assert.equal(toObject(2).toString(), "2")
     assert.equal(toObject(5.5).toString(), "5.5")
     assert.equal(toObject(-3).toString(), "-3")
-  })
-
-  it("allows toString usage in the language", () => {
-    assertExpression("1.toString()", toObject("1"))
-    assertExpression("1.5.toString()", toObject("1.5"))
-    assertExpression("-3.toString()", toObject("-3"))
   })
 })
 
@@ -60,24 +17,9 @@ describe("String", () => {
   it("supports toString to get the Javascript value", () => {
     assert.equal(toObject("Hello").toString(), "Hello")
   })
-
-  it("supports string concatenation", () => {
-    assertExpression(`"Hello " + "World"`, toObject("Hello World"))
-  })
 })
 
 describe("Array", () => {
-  it("allows initialization of a list of values", () => {
-    let tests = {
-      "[].length()": toObject(0),
-      "[1,2,3].length()": toObject(3)
-    }
-
-    for(var test in tests) {
-      assertExpression(test, tests[test])
-    }
-  })
-
   it("can convert to and from a javascript array", () => {
     let before = [1, 2, 3]
     let after = toObject(before)
@@ -85,28 +27,6 @@ describe("Array", () => {
     assert.equal(after.data[0].data, 1)
     assert.equal(after.data[1].data, 2)
     assert.equal(after.data[2].data, 3)
-  })
-
-  it("allows pushing and popping values", () => {
-    let tests = {
-      "a = []; a.push(1); a.length()": toObject(1),
-      "[1].pop()": toObject(1)
-    }
-
-    for(var test in tests) {
-      assertExpression(test, tests[test])
-    }
-  })
-
-  it("can iterate over the array's entries", () => {
-    let test = `
-      a = [1, 2, 3]
-      accum = 0
-      a.each({ |num| accum = accum + num })
-      accum
-    `
-
-    assertExpression(test, toObject(6))
   })
 })
 

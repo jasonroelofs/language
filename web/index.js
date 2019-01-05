720918a346b060079a2168bd9eec0847c3504e4b
Jason Roelofs <jasongroelofs@gmail.com>
2019-01-05 18:47:13 -0500

    Use the available toString() message on the web
    Also fix a weird infinite loop bug with ActivationRecord.toString.

--- web/index.js
+++ web/index.js
@@ -1,15 +1,21 @@
 import VM from "vm/vm.js"
+import { SendMessage, ToObject } from "vm/object.js"
 
 var vm = new VM()
 
 var codeEl = document.getElementById("code")
 var resultsEl = document.getElementById("results")
 
+function getStringOf(obj) {
+  let toString = SendMessage(obj, ToObject("toString"))
+  return vm.evalBlockWithArgs(obj, toString)
+}
+
 codeEl.addEventListener("input", (e) => {
   try {
     var results = vm.eval(codeEl.value, "[input]")
 
-    resultsEl.innerHTML = results.toString()
+    resultsEl.innerHTML = getStringOf(results).data
   } catch(error) {
     console.log(error)
   }
